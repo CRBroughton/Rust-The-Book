1d41257d6474909966b808f07752db4e2504638d
CRBroughton <crbroughton@posteo.uk>
2022-06-03 10:43:34 +0100

    add reference example

--- references-and-borrowing/src/main.rs
+++ references-and-borrowing/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let s1 = String::from("hello world");
+    let len = calculate_length(&s1); // Similar to C, passes without giving up ownership
+
+    println!("The length of '{}' is {}.", s1, len);
+}
+
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
 }
