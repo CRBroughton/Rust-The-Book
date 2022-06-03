48b1e76b3ed2b8d7b8874a7a1b11d480da7d0767
CRBroughton <crbroughton@posteo.uk>
2022-06-03 14:19:00 +0100

    create ipAddrKind enum and ipAddr struct

--- structs/enums/src/main.rs
+++ structs/enums/src/main.rs
@@ -1,3 +1,15 @@
-fn main() {
-    println!("Hello, world!");
+enum IpAddrKind {
+    V4(String),
+    V6(String)
 }
+
+struct IpAddr {
+    kind: IpAddrKind,
+    address: String
+}
+
+fn main() {
+    let localhost = IpAddrKind::V4(String::from("127.0.0.1"));
+}
+
+fn rout(ip_kind: IpAddrKind) {}
