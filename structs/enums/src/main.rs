20ab222b5d9ceabe23f4a2a23414c2468cfff0d9
CRBroughton <crbroughton@posteo.uk>
2022-06-03 14:34:55 +0100

    written match function

--- structs/enums/src/main.rs
+++ structs/enums/src/main.rs
@@ -24,10 +24,23 @@ struct IpAddr {
 fn main() {
     // let localhost = IpAddrKind::V4(127, 0, 0, 1);
 
-    let x = 5;
-    let y: Option<i32> = None;
+    // let x = 5;
+    // let y: Option<i32> = None;
 
-    let sum = x + y.unwrap_or(0);
+    // let sum = x + y.unwrap_or(0);
+
+
+    let five = Some(5);
+    let six = plus_one(five);
+    let none = plus_one(None);
 }
 
 fn route(ip_kind: IpAddrKind) {}
+
+
+fn plus_one(x: Option<i32>) -> Option<i32> { // match expression
+    match x {
+        None => None,
+        Some(i) => Some(i + 1),
+    }
+}
