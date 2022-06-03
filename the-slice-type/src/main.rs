8b2ae650bbf3d681edf85391331ea0a165bc4099
CRBroughton <crbroughton@posteo.uk>
2022-06-03 12:21:47 +0100

    finished slices section

--- the-slice-type/src/main.rs
+++ the-slice-type/src/main.rs
@@ -1,16 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("hello world");
+    let word = first_word(&s);
+
+    println!("the first word is: {}", word);
 }
 
 
-fn first_word(s: &String) -> usize {
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
 
 
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
-            return i;
+            return &s[0..i];
         }
     }
-    s.len()
+    &s[..]
 }
