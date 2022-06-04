9b2307eafb72e14eecab1057b84ce5dd7fff63d7
CRBroughton <crbroughton@posteo.uk>
2022-06-04 10:01:01 +0100

    add boilerplate for traits

---
+++ traits/src/main.rs
@@ -0,0 +1,16 @@
+pub struct NewsArticle {
+    pub author: String,
+    pub headline: String,
+    pub content: String,
+}
+
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+fn main() {
+    println!("Hello, world!");
+}
