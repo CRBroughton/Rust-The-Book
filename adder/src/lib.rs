2f33a3b2837da0433f2fc8f2e64461099e6dac1c
CRBroughton <crbroughton@posteo.uk>
2022-06-04 11:04:55 +0100

    add greeting fn and test

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -14,6 +14,10 @@ impl Rectangle {
 pub fn add_two(a: i32) -> i32 {
     a + 2
 }
+
+pub fn greeting(name: &str) -> String {
+    format!("Hello {}", name)
+}
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -36,5 +40,10 @@ mod tests {
     #[test]
     fn it_adds_two() {
         assert_eq!(4, add_two(2));
+    }
+    #[test]
+    fn greeting_contains_name() {
+        let result = greeting("Craig");
+        assert!(result.contains("Craig"));
     }
 }
