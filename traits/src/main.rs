703cb9238ae2c6d9ec5f9fff1c621b24cdf7c54c
CRBroughton <crbroughton@posteo.uk>
2022-06-04 10:03:20 +0100

    add impl block for NewsArticle

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -4,6 +4,12 @@ pub struct NewsArticle {
     pub content: String,
 }
 
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{} by {}", self.headline, self.author)
+    }
+}
+
 pub struct Tweet {
     pub username: String,
     pub content: String,
