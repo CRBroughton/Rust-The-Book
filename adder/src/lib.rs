4f1c3e3e0fc0ca945875b30a361ce53694240820
CRBroughton <crbroughton@posteo.uk>
2022-06-04 10:54:08 +0100

    add Rectangle struct and impl

--- adder/src/lib.rs
+++ adder/src/lib.rs
@@ -1,5 +1,20 @@
 #[cfg(test)]
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width == other.width && self.height == other.height
+    }
+}
+
 mod tests {
+    use core::panic;
+
     #[test]
     fn it_works() {
         let result = 2 + 2;
