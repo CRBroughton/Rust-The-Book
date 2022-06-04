4578978a1b67eab664d8ae9c5364c7de00f25760
CRBroughton <crbroughton@posteo.uk>
2022-06-04 12:24:28 +0100

    moved run and Config to lib.rs

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -19,28 +19,3 @@ fn main() {
         process::exit(1);
     }
 }
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-
-    println!("With text:\n{}", contents);
-
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Config { query, filename })
-    }
-}
