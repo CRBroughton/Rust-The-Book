f50d4373634272681f06afd3b426988f9bd731f5
CRBroughton <crbroughton@posteo.uk>
2022-06-26 16:28:11 +0100

    create interator impl

--- iterator/src/main.rs
+++ iterator/src/main.rs
@@ -7,6 +7,20 @@ impl Counter {
         Counter { count: 0 }
     }
 }
+
+impl Iterator for Counter {
+    type Item = u32;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.count < 5 {
+            self.count += 1;
+            Some(self.count)
+        } else {
+            None
+        }
+    }
+}
+
 fn main() {
     // let v1 = vec![1, 2, 3];
 
