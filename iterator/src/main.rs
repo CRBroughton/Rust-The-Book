049b5453d690c055f003492085fd3073f3765f71
CRBroughton <crbroughton@posteo.uk>
2022-06-26 16:29:44 +0100

    add test function for Counter

--- iterator/src/main.rs
+++ iterator/src/main.rs
@@ -21,6 +21,18 @@ impl Iterator for Counter {
     }
 }
 
+#[test]
+fn calling_next_directly() {
+    let mut counter = Counter::new();
+
+    assert_eq!(counter.next(), Some(1));
+    assert_eq!(counter.next(), Some(2));
+    assert_eq!(counter.next(), Some(3));
+    assert_eq!(counter.next(), Some(4));
+    assert_eq!(counter.next(), Some(5));
+    assert_eq!(counter.next(), None);
+}
+
 fn main() {
     // let v1 = vec![1, 2, 3];
 
