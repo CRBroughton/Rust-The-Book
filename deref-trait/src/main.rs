87c3a58c92434e27c9e290abd354544b6d2db5f3
CRBroughton <crbroughton@posteo.uk>
2022-07-02 14:44:49 +0100

    replaced Box with MyBox

--- deref-trait/src/main.rs
+++ deref-trait/src/main.rs
@@ -7,7 +7,7 @@ impl<T> MyBox<T> {
 }
 fn main() {
     let x  = 5;
-    let y = Box::new(x); // Reference to x
+    let y = MyBox::new(x); // Reference to x
 
     assert_eq!(5, x);
     assert_eq!(5, *y); // deref
