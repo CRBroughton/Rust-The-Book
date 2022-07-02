aca76e4e98f5374c829adb278251f4e86a749671
CRBroughton <crbroughton@posteo.uk>
2022-07-02 14:46:15 +0100

    create Deref impl block with deref fn

--- deref-trait/src/main.rs
+++ deref-trait/src/main.rs
@@ -7,6 +7,15 @@ impl<T> MyBox<T> {
         MyBox(x)
     }
 }
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
 fn main() {
     let x  = 5;
     let y = MyBox::new(x); // Reference to x
