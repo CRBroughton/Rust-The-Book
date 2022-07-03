5928689f88881c497f33632993eb78c525c135fe
CRBroughton <crbroughton@posteo.uk>
2022-07-03 12:11:38 +0100

    impl drop fn for CustomSmartPointer

--- drop-trait/src/main.rs
+++ drop-trait/src/main.rs
@@ -2,6 +2,12 @@ struct CustomSmartPointer {
     data: String
 }
 
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data: {}", self.data);
+    }
+}
+
 fn main() {
     println!("Hello, world!");
 }
