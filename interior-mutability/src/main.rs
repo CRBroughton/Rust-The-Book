93ce9212ea8866099d8486f4cf0de6b84e30b409
CRBroughton <crbroughton@posteo.uk>
2022-07-04 19:31:23 +0100

    create LimitTracker struct

--- interior-mutability/src/main.rs
+++ interior-mutability/src/main.rs
@@ -2,6 +2,12 @@ pub trait Messenger {
     fn send(&self, msg: &str);
 }
 
+pub struct LimitTracker<'a, T: Messenger> {
+    messenger: &'a T,
+    value: usize,
+    max: usize,
+}
+
 fn main() {
     let x = 5;
     let y = &mut x;
