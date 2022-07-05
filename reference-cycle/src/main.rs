cdc546eb7f15be630995e1f107b33098c081942b
CRBroughton <crbroughton@posteo.uk>
2022-07-05 20:22:51 +0100

    add main fn, fixed Node struct

--- reference-cycle/src/main.rs
+++ reference-cycle/src/main.rs
@@ -50,5 +50,17 @@ use std::rc::Rc;
 #[derive(Debug)]
 struct Node {
     value: i32,
-    children: RefCell<Vec<Node>>,
+    children: RefCell<Vec<Rc<Node>>>,
+}
+
+fn main() {
+    let leaf = Rc::new(Node {
+        value: 3,
+        children: RefCell::new(vec![]),
+    });
+
+    let branch = Rc::new(Node {
+        value: 5,
+        children: RefCell::new(vec![Rc::clone(&leaf)]),
+    });
 }
