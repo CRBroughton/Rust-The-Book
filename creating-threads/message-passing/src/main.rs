0bd72ad8adf1577ceefddf77a3fc9c119ffff9a5
CRBroughton <crbroughton@posteo.uk>
2022-07-10 15:04:04 +0100

    created new thread

--- creating-threads/message-passing/src/main.rs
+++ creating-threads/message-passing/src/main.rs
@@ -1,5 +1,11 @@
 use std::sync::mpsc;
+use std::thread;
 
 fn main() {
     let (tx,rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let msg = String::from("hi");
+        tx.send(msg).unwrap();
+    });
 }
